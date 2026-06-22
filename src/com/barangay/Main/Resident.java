93bae2a4913ee0211a6bbb5a2b89742c9182af89
GabrielAcuin <gabriel.acuin.college@ustangelicum.edu.ph>
2026-06-22 21:27:36 +0800

    creeated residentName class in domain class

--- src/com/barangay/Main/Resident.java
+++ src/com/barangay/Main/Resident.java
@@ -1,5 +1,36 @@
 package com.barangay.Main;
 
-public interface Resident{
+interface Resident{
+    public void getName();
+}
+
+class residentName{
+    private String firstName;
+    private String lastName;
+
+    public residentName(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public String getFirstName(){
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getlastName(){
+
+        return lastName;
+
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
 
 }
