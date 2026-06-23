8f5c418759c962f5dabd720841ea828ae51bc3c9
Aldric Justin, Cadiang <aldric.cadiang.college@ustangelicum.edu.ph>
2026-06-23 23:09:38 +0800

    Updated Main.java (In Main Folder) with new custom exception , and updated try catch and overall update to content of menu

--- src/com/barangay/Main/Main.java
+++ src/com/barangay/Main/Main.java
@@ -1,35 +1,43 @@
 package com.barangay.Main;
+import com.barangay.models.CustomException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        
+            while (true) {
+                System.out.println("\nSelect an option:");
+                System.out.println("1: Option 1 - Resident");
+                System.out.println("2: Option 2 - Gcash");
+                System.out.println("3: Option 3 -");
+                System.out.println("4: Exit");
+                System.out.print("Enter your choice: ");
 
-        System.out.println("Select an option:");
-        System.out.println("1: ");
-        System.out.println("2: ");
-        System.out.println("3: ");
-        System.out.println("3: Exit ");
-        System.out.print("Enter your choice  ");
+                int choice;
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
 
-        try {
-            int choice = scanner.nextInt();
+                    if (choice == 1) {
+                        System.out.println("You selected Option 1 - Resident");
+                    } else if (choice == 2) {
+                        System.out.println("You selected Option 2 - Gcash");
+                    } else if (choice == 3) {
+                        System.out.println("You selected Option 3 - ");
 
-            if (choice == 1) {
-                System.out.println("");
-            } else if (choice == 2) {
-                System.out.println("");
-            } else if (choice == 3) {
-                System.out.println(" ");
-            } else if (choice == 4) {
-                System.out.println("Goodbye!");
-            } else {
-                System.out.println("Invalid choice! .");
+                    } else if (choice == 4) {
+                        System.out.println("Goodbye!");
+                        System.out.println("Program terminated.");
+                        scanner.close();
+                        break;
+                    } else {
+                        throw new CustomException("Invalid choice! Please select 1, 2, 3, or 4.");                    }
+                } catch (NumberFormatException e) {
+                System.out.println("Error: Input must be among the given choises!");
+            } catch (CustomException e) {
+                System.out.println("Error: " + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println("Invalid input! ");
-        } finally {
-            scanner.close();
         }
     }
 }
