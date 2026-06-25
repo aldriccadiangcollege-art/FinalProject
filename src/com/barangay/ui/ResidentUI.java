ac4d1be65d08c37b2b19030c0b26f2b7f64901a6
GabrielAcuin <gabriel.acuin.college@ustangelicum.edu.ph>
2026-06-25 17:28:55 +0800

    Created ResidentUI.java for User inputs for Residents

---
+++ src/com/barangay/ui/ResidentUI.java
@@ -0,0 +1,11 @@
+package com.barangay.ui;
+
+import java.util.Scanner;
+
+public class ResidentUI {
+    private Scanner scanner;
+
+    public ResidentUI(Scanner scanner){
+        this.scanner = scanner;
+    }
+}
