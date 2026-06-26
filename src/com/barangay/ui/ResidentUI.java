9d42de3744eb5f7d663930653c13ea541a172f21
GabrielAcuin <gabriel.acuin.college@ustangelicum.edu.ph>
2026-06-26 22:53:50 +0800

    Coded a registration to get name, birthdate, and gender
    encountered a few errors, may need help

--- src/com/barangay/ui/ResidentUI.java
+++ src/com/barangay/ui/ResidentUI.java
@@ -1,11 +1,41 @@
 package com.barangay.ui;
 
 import java.util.Scanner;
+import com.barangay.models.*;
+import java.time.LocalDate;
 
 public class ResidentUI {
-    private Scanner scanner;
+    private Scanner sc = new Scanner(System.in);
 
-    public ResidentUI(Scanner scanner){
-        this.scanner = scanner;
+    public Resident registerResident(){
+        System.out.println("First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.println("Last Name: ");
+        String lastName = sc.nextLine();
+        residentName name = new residentName(firstName, lastName);
+
+        System.out.println("Birthdate: ");
+        LocalDate birthdate = LocalDate.parse(sc.nextLine());
+        residentBirthDate birth = new residentBirthDate(birthdate);
+        residentBdate bDate = new residentBDate(birth);
+
+        System.out.print("Gender (M/F/X): ");
+        String gender Input = sc.nextLine();
+        ResidentGender gender;
+        switch (genderInput.toUpperCase()){
+            case "M": gender = new Male(); break;
+            case "F": gender = new Female(); break; 
+            default: gender = new PreferNotToSay(); break; 
+        }
+
+        System.out.println("Resident: " + name.getFirstName() + " " + name.getlastName() +
+                          ", Birthday" + bDate.getResidentBirthDay() + 
+                          ", Gender: " + gender.getResidentGender());
+            return null;
     }
+
+
 }
+
+
