12d41a5ff18a85f4b689a9165b9ed12362874e43
GabrielAcuin <gabriel.acuin.college@ustangelicum.edu.ph>
2026-06-26 23:16:49 +0800

    added code for searching by name, needs to be worked on due to errors.

--- src/com/barangay/ui/ResidentUI.java
+++ src/com/barangay/ui/ResidentUI.java
@@ -1,12 +1,14 @@
 package com.barangay.ui;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import com.barangay.models.*;
 import java.time.LocalDate;
 
 public class ResidentUI {
     private Scanner sc = new Scanner(System.in);
-
+    //registration
     public Resident registerResident(){
         System.out.println("First Name: ");
         String firstName = sc.nextLine();
@@ -28,14 +30,71 @@ public class ResidentUI {
             case "F": gender = new Female(); break; 
             default: gender = new PreferNotToSay(); break; 
         }
-
+        
         System.out.println("Resident: " + name.getFirstName() + " " + name.getlastName() +
-                          ", Birthday" + bDate.getResidentBirthDay() + 
-                          ", Gender: " + gender.getResidentGender());
-            return null;
+        ", Birthday" + bDate.getResidentBirthDay() + 
+        ", Gender: " + gender.getResidentGender());
+        return null;
+        
+    }
+    
+    
+}
+
+
+
+public class RegistrationSystem {
+    private List<residentName> residents = new ArrayList<>();
+
+    // Add a resident
+    public void registerResident(residentName name) {
+        residents.add(name);
     }
 
+    // Search by first + last name
+    public residentName searchByName(String firstName, String lastName) {
+        for (residentName r : residents) {
+            if (r.getFirstName().equalsIgnoreCase(firstName) &&
+                r.getlastName().equalsIgnoreCase(lastName)) {
+                return r;
+            }
+        }
+        return null; // not found
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RegistrationSystem system = new RegistrationSystem();
+
+        System.out.print("How many residents to register? ");
+        int count = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("First Name: ");
+            String firstName = sc.nextLine();
+            System.out.print("Last Name: ");
+            String lastName = sc.nextLine();
+
+            residentName name = new residentName(firstName, lastName);
+            system.registerResident(name);
+        }
+
+        // Search
+        System.out.print("\nEnter name to search (First Last): ");
+        String[] searchInput = sc.nextLine().split(" ");
+        residentName found = system.searchByName(searchInput[0], searchInput[1]);
+
+        if (found != null) {
+            System.out.println("Resident found: " + found.getFirstName() + " " + found.getlastName());
+        } else {
+            System.out.println("Resident not found.");
+        }
+
+        sc.close();
+    }
 }
+
+    
 
 
